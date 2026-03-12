a546a5ef35e071cbc687d87d1fc9a7e92603165a
Mark Brezina <77184504+MarkBrezina@users.noreply.github.com>
2026-03-12 08:52:38 +0000

    Update portfolio_optimizers.py

--- src/portfolio_optimizers.py
+++ src/portfolio_optimizers.py
@@ -1,1 +1,12 @@
 
+import numpy as np
+
+def min_variance_portfolio(cov):
+
+    inv = np.linalg.inv(cov)
+
+    ones = np.ones(len(cov))
+
+    w = inv @ ones / (ones.T @ inv @ ones)
+
+    return w
